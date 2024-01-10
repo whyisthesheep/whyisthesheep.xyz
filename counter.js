184ffd4417b35bf9e2ef576be90e20859b9deed4
whyisthesheep <leonjagger@icloud.com>
2024-01-10 20:09:07 +0000

    Commit

--- counter.js
+++ counter.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const startDate = new Date('2024-01-10'); // Replace with your start date
+    const startDate = new Date('2024-01-10');
     const currentDate = new Date();
     const timeDiff = currentDate - startDate;
-    const daysSince = Math.floor(timeDiff / (1000 * 60 * 60 * 24)); // Convert milliseconds to days
+    const daysSince = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
 
     document.getElementById('daysCounter').innerText = daysSince + ' days since the start date';
 });
