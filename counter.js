950a921c8f7dfd3efef1a5428177c03f571b65e3
whyisthesheep <leonjagger@icloud.com>
2024-01-12 20:31:05 +0000

    V2

--- counter.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const startDate = new Date('2024-01-10');
-    const currentDate = new Date();
-    const timeDiff = currentDate - startDate;
-    const daysSince = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-
-    document.getElementById('daysCounter').innerText = daysSince + ' days since the start date';
-});
