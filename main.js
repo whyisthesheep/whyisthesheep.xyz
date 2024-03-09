0204f3cccbe4d953d4234286fc3a792a724a15b0
whyisthesheep <whyisthesheep@disroot.org>
2024-03-09 13:28:26 +0000

    update

---
+++ main.js
@@ -0,0 +1,10 @@
+console.log("#####################")
+console.log("#                   #")
+console.log("#   Run helpme()    #")
+console.log("#  if you are cool  #")
+console.log("#                   #")
+console.log("#####################")
+
+function helpme() {
+    console.log("Coming soon (im sorry)")
+}
